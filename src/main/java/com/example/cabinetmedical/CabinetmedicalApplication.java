fc61224e40615acb7f1ad54ab4ee58139bb331b3
VisualVibe <Visual.vibe1999@gmail.com>
2025-11-18 17:22:20 +0100

    first commit

---
+++ src/main/java/com/example/cabinetmedical/CabinetmedicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinetmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetmedicalApplication.class, args);
+	}
+
+}
