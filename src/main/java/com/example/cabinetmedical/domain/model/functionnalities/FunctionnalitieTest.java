de3c055e51656a07d6e33f33ba437247745d79c4
VisualVibe <Visual.vibe1999@gmail.com>
2025-11-29 17:26:40 +0100

    Functionnalite du test

---
+++ src/main/java/com/example/cabinetmedical/domain/model/functionnalities/FunctionnalitieTest.java
@@ -0,0 +1,16 @@
+package com.example.cabinetmedical.domain.model.functionnalities;
+
+import com.example.cabinetmedical.domain.utils.FeatureParameter;
+import com.example.cabinetmedical.domain.utils.FeatureResponce;
+
+public class FunctionnalitieTest implements Functionnalitie {
+
+  
+    @Override
+    public FeatureResponce performWork(FeatureParameter parameter) {
+        // Implémentation de la méthode performWork pour le test
+        return  null; // Retourne une instance vide pour le test
+    }
+
+    
+}
