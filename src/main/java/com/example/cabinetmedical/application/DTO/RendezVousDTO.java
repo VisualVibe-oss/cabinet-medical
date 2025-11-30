d5c659d1f208c623f37376a10082f7667370d093
AymanFaragi <aymane.faragi@gmail.com>
2025-11-30 22:31:51 +0100

    Creation de DTO du rendez vous

---
+++ src/main/java/com/example/cabinetmedical/application/DTO/RendezVousDTO.java
@@ -0,0 +1,19 @@
+package com.example.cabinetmedical.application.DTO;
+
+import com.example.cabinetmedical.domain.model.Cabinet.Cabinet;
+import com.example.cabinetmedical.domain.model.patient.Patient;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class RendezVousDTO {
+    private int idRendezVous;
+    private Date dateRendezVous;
+    private String motif;
+    private String statut;
+    private String notes;
+    private Cabinet cabinet;
+    private Patient patient;
+
+}
