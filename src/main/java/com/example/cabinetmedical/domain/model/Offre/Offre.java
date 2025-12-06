667017ecb56206e939588935ce0f6dc683f66c2c
VisualVibe <Visual.vibe1999@gmail.com>
2025-12-06 11:36:09 +0100

    Ajouter les attribut ( packey  , periode du abnoment en jour )

--- src/main/java/com/example/cabinetmedical/domain/model/Offre/Offre.java
+++ src/main/java/com/example/cabinetmedical/domain/model/Offre/Offre.java
@@ -3,30 +3,28 @@ package com.example.cabinetmedical.domain.model.Offre;
 import java.util.ArrayList;
 import java.util.List;
 import com.example.cabinetmedical.domain.utils.Featurekey;
+import com.example.cabinetmedical.domain.utils.PackKey;
 
 //! A verifier le nom si il doit etre offre ou abonement
 //* cette classe cet la version domaine du class OffreEntity qui existe en base de données */
 //! A verifier les attributs de la classe 
 public class Offre {
-    private List<Featurekey> featurekeys = new ArrayList<>();  
+    private PackKey packKey ; 
 
     // Constructeur sans-argument
-    public Offre() {
-        this.featurekeys = new ArrayList<>();
+    public Offre(PackKey packKey) {
+       this.packKey = packKey ;
     }
 
-    // Constructeur avec liste de Featurekey
-    public Offre(List<Featurekey> featurekeys) {
-        this.featurekeys = featurekeys == null ? new ArrayList<>() : featurekeys;
-    }
+    
 
     // Getter
-    public List<Featurekey> getFeaturekeys() {
-        return featurekeys;
+    public PackKey getPackkey() {
+        return packKey;
     }
 
     // Setter
-    public void setFeaturekeys(List<Featurekey> featurekeys) {
-        this.featurekeys = featurekeys == null ? new ArrayList<>() : featurekeys;
+    public void setPackkey(PackKey packKey) {
+        this.packKey = packKey ;
     }
 }
