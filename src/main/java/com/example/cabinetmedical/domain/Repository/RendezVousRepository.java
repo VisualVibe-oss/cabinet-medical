a7a235c99d76dfbe0fda2691559ef0275aa95c08
AymanFaragi <aymane.faragi@gmail.com>
2025-12-09 21:44:03 +0100

    changes/fixes to existing repositories

--- src/main/java/com/example/cabinetmedical/domain/Repository/RendezVousRepository.java
+++ src/main/java/com/example/cabinetmedical/domain/Repository/RendezVousRepository.java
@@ -1,6 +1,6 @@
 package com.example.cabinetmedical.domain.Repository;
 
-import com.example.cabinetmedical.domain.model.rendezvous.RendezVous;
+import com.example.cabinetmedical.domain.model.RendezVous.RendezVous;
 import com.example.cabinetmedical.infrastructure.entity.RendezVousEntity;
 
 public interface RendezVousRepository {
