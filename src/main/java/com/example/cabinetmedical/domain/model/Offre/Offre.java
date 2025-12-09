2d2c211e999e793505c565b5825b600a582f7b8d
AymanFaragi <aymane.faragi@gmail.com>
2025-12-09 21:47:48 +0100

    adding a offertype enum class and changing the offer type in the offer domain model

--- src/main/java/com/example/cabinetmedical/domain/model/Offre/Offre.java
+++ src/main/java/com/example/cabinetmedical/domain/model/Offre/Offre.java
@@ -3,12 +3,14 @@ package com.example.cabinetmedical.domain.model.Offre;
 import java.util.ArrayList;
 import java.util.List;
 import com.example.cabinetmedical.domain.utils.Featurekey;
+import com.example.cabinetmedical.domain.utils.OfferType;
 
 //! A verifier le nom si il doit etre offre ou abonement
 //* cette classe cet la version domaine du class OffreEntity qui existe en base de données */
 //! A verifier les attributs de la classe 
 public class Offre {
-    private List<Featurekey> featurekeys = new ArrayList<>();  
+    private List<Featurekey> featurekeys = new ArrayList<>();
+    private OfferType type;
 
     // Constructeur sans-argument
     public Offre() {
@@ -29,4 +31,6 @@ public class Offre {
     public void setFeaturekeys(List<Featurekey> featurekeys) {
         this.featurekeys = featurekeys == null ? new ArrayList<>() : featurekeys;
     }
+
+    public OfferType getType() {return this.type;}
 }
