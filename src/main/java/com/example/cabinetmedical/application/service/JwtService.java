6794c8aae993a7f2526a9f0813586133afced50b
VisualVibe <Visual.vibe1999@gmail.com>
2025-12-17 16:26:55 +0100

    Ajouter  le provider  utilser par le filtre de jwt

---
+++ src/main/java/com/example/cabinetmedical/application/service/JwtService.java
@@ -0,0 +1,132 @@
+package com.example.cabinetmedical.application.service;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import com.example.cabinetmedical.application.dto.UserDTO;
+import com.example.cabinetmedical.infrastructure.entity.RefreshTokenEntity;
+import com.example.cabinetmedical.infrastructure.repository.RefreshTokenRepository;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.transaction.Transactional;
+import io.jsonwebtoken.Claims;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    @Value("${jwt.access.secret}")
+    private String accessSecret;
+
+    @Value("${jwt.access.expiration}")
+    private long accessExpiration;
+
+    @Value("${jwt.refresh.secret}")
+    private String refreshSecret;
+
+    @Value("${jwt.refresh.expiration}")
+    private long refreshExpiration;
+
+    // generer le token par id
+    public String generateAccesToken(UserDTO user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + accessExpiration);
+
+        Key key = Keys.hmacShaKeyFor(accessSecret.getBytes());
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // * cette methode supprime l'ancien refreshToken et generer un nouveau */
+    public String generateAndSaveRefreshToken(UserDTO user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + refreshExpiration);
+        Key key = Keys.hmacShaKeyFor(refreshSecret.getBytes());
+        String refreshToken = Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        refreshTokenRepository.deleteByEmail(user.getEmail());
+        RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity();
+        refreshTokenEntity.setRefreshToken(refreshToken);
+        refreshTokenEntity.setEmail(user.getEmail());
+        refreshTokenEntity.setExpiryDate(expiryDate);
+
+        refreshTokenRepository.save(refreshTokenEntity);
+
+        return refreshToken;
+    }
+
+    @Transactional
+    private void validateRefreshToken(String refreshToken) {
+
+        // 1️⃣ Vérifier signature + structure JWT
+        Key key = Keys.hmacShaKeyFor(refreshSecret.getBytes());
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(refreshToken)
+                .getBody();
+
+        // 2️⃣ Récupérer l’email depuis le token
+        String email = claims.getSubject();
+
+        // 3️⃣ Vérifier présence en base
+        RefreshTokenEntity tokenEntity = refreshTokenRepository
+                .findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new BadCredentialsException("Refresh token inexistant"));
+
+        // 4️⃣ Vérifier expiration (JWT + base)
+        Date now = new Date();
+        if (tokenEntity.getExpiryDate().before(now)) {
+            refreshTokenRepository.delete(tokenEntity);
+            throw new BadCredentialsException("Refresh token expiré");
+        }
+
+    }
+
+    private UserDTO tokenToUserDTO(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(refreshSecret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        String email = claims.getSubject(); // récupère le sujet (email)
+        String role = claims.get("role", String.class);
+
+        UserDTO user = new UserDTO();
+        user.setEmail(email);
+        user.setRole(role);
+        return user;
+    }
+
+    public String refreshToken(String token) {
+
+        validateRefreshToken(token);
+        UserDTO user = tokenToUserDTO(token) ;
+        return  generateAccesToken(user);
+    }
+
+}
--- src/main/java/com/example/cabinetmedical/config/security/JwtTokenProvider.java
+++ src/main/java/com/example/cabinetmedical/config/security/JwtTokenProvider.java
@@ -1,69 +1,58 @@
 package com.example.cabinetmedical.config.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    @Value("${jwt.secret}")
-    private String jwtSecret;
 
-    @Value("${jwt.expiration}")
-    private long expiration;
+    @Value("${jwt.access.secret}")
+    private String jwtAccesSecret;
 
-    //generer le token par id
-    public String generateTokenWithId(int userId,String email,String role){
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + expiration);
+    @Value("${jwt.refresh.secret}")
+    private String jwtRefreshSecret;
 
-        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-        return Jwts.builder()
-                .setSubject(email)
-                .claim("userId",userId)
-                .claim("role",role)
-                .setIssuedAt(now)
-                .setExpiration(expiryDate)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
+    @Value("${jwt.access.expiration}")
+    private long accesExpiration;
 
-    //extraire l email du token
-    public String getEmailFromToken(String token){
-        Claims claims = Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes())
+    // extraire l email du token
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(jwtAccesSecret.getBytes())
                 .build().parseClaimsJws(token).getBody();
         return claims.getSubject();
     }
 
-    //extraire le role du token
-    public String getRoleFromToken(String token){
-        Claims claims=Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes())
+    // extraire le role du token
+    public String getRoleFromToken(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(jwtAccesSecret.getBytes())
                 .build().parseClaimsJws(token).getBody();
-        return claims.get("role",String.class);
+        return claims.get("role", String.class);
     }
 
-    //extraire id du token
-    public int getIdFromToken(String token){
-        Claims claims=Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes())
-                .build().parseClaimsJws(token).getBody();
-        return claims.get("userId",Integer.class);
-    }
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtAccesSecret.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
 
-    //validitite du token
-    public boolean validateToken(String token){
-        try{
-            Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes())
-                    .build().parseClaimsJws(token);
-            return true;
-        }catch(Exception e){
+            // Vérifier l'expiration
+            return claims.getExpiration().after(new Date());
+
+        } catch (ExpiredJwtException e) {
+            // Token expiré
+            return false;
+        } catch (JwtException | IllegalArgumentException e) {
+            // Token invalide (signature, format, etc.)
             return false;
         }
     }
+
 }
