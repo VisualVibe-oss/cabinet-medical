3f54e6ee9f12bcb8e62ea41ac6727ed9f534d27f
AymanFaragi <aymane.faragi@gmail.com>
2026-01-05 03:35:29 +0100

    necessary changes to existing rendez vous related logic, ensuring the correct assignment of patients and cabinets to rendez-vous types

--- src/main/java/com/example/cabinetmedical/application/service/RendezVousAppService.java
+++ src/main/java/com/example/cabinetmedical/application/service/RendezVousAppService.java
@@ -3,24 +3,62 @@ package com.example.cabinetmedical.application.service;
 import com.example.cabinetmedical.application.DTO.RendezVousDTO;
 import com.example.cabinetmedical.domain.model.RendezVous.RendezVous;
 import com.example.cabinetmedical.infrastructure.entity.RendezVousEntity;
+import com.example.cabinetmedical.infrastructure.mapper.CabinetMapper;
+import com.example.cabinetmedical.infrastructure.mapper.PatientMapper;
 import com.example.cabinetmedical.infrastructure.mapper.RendezVousMapper;
+import com.example.cabinetmedical.infrastructure.repository.CabinetRepository;
 import com.example.cabinetmedical.infrastructure.repository.RendezVous.RendezVousRepositoryImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RendezVousAppService {
 
     private RendezVousRepositoryImpl rendezVousRepository;
+    private CabinetRepository cabinetRepository;
     private RendezVousMapper rvm;
+    private CabinetMapper cm;
+    private PatientMapper pm;
 
-    public RendezVousAppService(RendezVousRepositoryImpl rendezVousRepository ) {
+    public RendezVousAppService(RendezVousRepositoryImpl rendezVousRepository, CabinetRepository cabinetRepository, RendezVousMapper rvm, CabinetMapper cm, PatientMapper pm ) {
         this.rendezVousRepository = rendezVousRepository;
+        this.cabinetRepository = cabinetRepository;
+        this.rvm = rvm;
+        this.cm = cm;
+        this.pm = pm;
     }
 
-    public RendezVousDTO create(RendezVous rv){
+    public RendezVousDTO create(RendezVous rv, int idCabinet) {
 
         RendezVousEntity rve = rvm.toEntity(rv);
+        rve.setCabinet(cabinetRepository.findByIdCabinet(idCabinet));
+        rve.setPatient(pm.toEntity(rv.getPatient()));
+        rendezVousRepository.save(rve);
+        RendezVousDTO rvdto = rvm.toDTO(rve);
+        rvdto.setIdCabinet(idCabinet);
+        rvdto.setPatient(rv.getPatient());
 
-        return rvm.toDTO(rendezVousRepository.save(rve));
+
+        return rvdto;
+    }
+
+    public List<RendezVousDTO> getAllRendezVous(int idCabinet){
+        List<RendezVousEntity> rves = rendezVousRepository.findAllByCabinet_idCabinet(idCabinet);
+        List<RendezVousDTO> rvs = rvm.toDTOList(rves);
+        for (int i = 0; i < rvs.size(); i++) {
+            RendezVousEntity entity = rves.get(i);
+            RendezVousDTO dto = rvs.get(i);
+
+            if (entity.getPatient() != null) {
+                dto.setPatient(pm.toDomain(entity.getPatient()));
+            }
+        }
+        System.out.println("getAllRendezVous "+rvs);
+        return rvs;
+    }
+
+    public RendezVous getRendezVous(int idRendezVous){
+        return rvm.toDomain(rendezVousRepository.findById(idRendezVous));
     }
 }
