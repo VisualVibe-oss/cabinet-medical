78e170d79e0624db8f810d60711f1c4a013e2cf1
VisualVibe <Visual.vibe1999@gmail.com>
2026-01-06 13:38:51 +0100

    changer des info dans le point d'entre de  l'application pour avoir charger les variable du .env

--- src/main/java/com/example/cabinetmedical/CabinetmedicalApplication.java
+++ src/main/java/com/example/cabinetmedical/CabinetmedicalApplication.java
@@ -3,10 +3,19 @@ package com.example.cabinetmedical;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CabinetmedicalApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// 2. Injecter chaque variable dans les propriétés système
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(CabinetmedicalApplication.class, args);
 	}
 
