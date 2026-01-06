8a73a685ebf467aadf8090870c776131ba4b17d9
VisualVibe <Visual.vibe1999@gmail.com>
2026-01-06 13:41:39 +0100

    ajouter la classe qui  verifie les utilsateur  et l etat du cabinet qui va etre ajoute au filtre

---
+++ src/main/java/com/example/cabinetmedical/application/service/CustomUserDetailsService.java
@@ -0,0 +1,66 @@
+package com.example.cabinetmedical.application.service;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.cabinetmedical.exception.CabinetBlockedExecption;
+import com.example.cabinetmedical.infrastructure.entity.MedecinEntity;
+import com.example.cabinetmedical.infrastructure.entity.SecretaireEntity;
+import com.example.cabinetmedical.infrastructure.repository.CabinetRepository;
+import com.example.cabinetmedical.infrastructure.repository.MedecinRepository;
+import com.example.cabinetmedical.infrastructure.repository.Secretaire.SpringSecretaireRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MedecinRepository medecinRepository;
+    private final SpringSecretaireRepository secretaireRepository;
+    private final CabinetRepository cabinetRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        String emailLower = email.toUpperCase().trim();
+
+
+            Optional<MedecinEntity> medecin = medecinRepository.findByEmail(emailLower);
+            if (medecin.isPresent()) {
+                MedecinEntity m = medecin.get();
+                boolean etat = cabinetRepository.findEtatByMedecin(m) ;
+                if(etat){
+                    return User.builder()
+                        .username(m.getEmail())
+                        .password(m.getPassword())
+                        .roles("MEDECIN")
+                        .build();
+                }
+                
+                throw new CabinetBlockedExecption("Cette Cabinet est bloquer") ;
+            }
+
+            Optional<SecretaireEntity> secretaire = secretaireRepository.findByEmail(emailLower);
+            if (secretaire.isPresent()) {
+                SecretaireEntity s = secretaire.get();
+                boolean etat = secretaireRepository.findEtatBySecretaireEmail(email) ;
+                if(etat){
+                    return User.builder()
+                        .username(s.getEmail())
+                        .password(s.getPassword())
+                        .roles("SECRETAIRE")
+                        .build();
+                }
+                
+                   throw new CabinetBlockedExecption("Cette Cabinet est bloquer") ;
+
+            }
+
+            throw new UsernameNotFoundException("Utilisateur non trouvé : " + email);
+        };
+}
