14adbd15ff36a4dd7823f5cc013a1cee2f07dcdf
VisualVibe <Visual.vibe1999@gmail.com>
2026-01-06 13:45:24 +0100

    Ajouter l'execption necessaire

---
+++ src/main/java/com/example/cabinetmedical/exception/CabinetBlockedExecption.java
@@ -0,0 +1,9 @@
+package com.example.cabinetmedical.exception;
+
+public class CabinetBlockedExecption  extends RuntimeException {
+
+    public CabinetBlockedExecption(String message) {
+        super(message);
+    }
+    
+}
